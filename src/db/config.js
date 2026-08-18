850e7d65bac8f255b672b9204c4b9a36fd460902
sharoon <kphsharoon@gmail.com>
2026-08-18 17:28:47 +0500

    feat: add Sequelize database configuration

--- src/db/config.js
+++ src/db/config.js
@@ -1,31 +1,24 @@
+import { Sequelize } from "sequelize";
 
-
-import { Sequelize } from 'sequelize';
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT,
+    }
+);
 
 const connectDB = async () => {
     try {
-        const sequelize = new Sequelize(
-            process.env.DB_NAME,
-            process.env.DB_USER,
-            process.env.DB_PASSWORD,
-            {
-                host: process.env.DB_HOST,
-                dialect: process.env.DB_DIALECT,
-            }
-        );
+        await sequelize.authenticate();
 
-        await sequelize.authenticate()
-            .then(() => {
-                console.log('Database connection has been established successfully.');
-            })
-            .catch((error) => {
-                throw error;
-            })
-
-    }
-    catch (error) {
-        console.error('Error connecting to the database:', error);
+        console.log("Database connected successfully");
+    } catch (error) {
+        console.error("Error connecting to the database:", error);
         throw error;
     }
-}
-export default connectDB;   
+};
+
+export { sequelize, connectDB };
