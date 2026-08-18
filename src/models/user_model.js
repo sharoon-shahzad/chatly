6b93f6f9694c972f60d1a771a86491f71bdf0fc2
sharoon <kphsharoon@gmail.com>
2026-08-18 17:30:55 +0500

    feat: define user Sequelize model

--- src/models/user_model.js
+++ src/models/user_model.js
@@ -1,42 +1,43 @@
-import { table, timeStamp } from 'node:console';
-import { type } from 'node:os';
-import sequelize from 'sequelize';
-import { DataTypes } from 'sequelize';
+import { DataTypes } from "sequelize";
+import { sequelize } from "../db/config.js";
 
-const User = sequelize.define('User', {
-    user_id: {
-        type: DataTypes.UUID,
-        primaryKey: true,
-        defaultValue: DataTypes.UUIDV4,
-        primaryKey: true,
-    },
-    username: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-        required: true,
-    },
-    email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-        required: true,
-    },
-    password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        required: true,
-
-    },
-    refresh_token: {
-        type: DataTypes.STRING,
-        allowNull: true,
-    }
-},
+const User = sequelize.define(
+    "User",
     {
-        tableName: 'users', // Specify the table name explicitly
-        timestamps: true, // Enable timestamps (createdAt and updatedAt)
-        createdAt: 'created_at', // Customize the name of the createdAt column
+        user_id: {
+            type: DataTypes.UUID,
+            primaryKey: true,
+            defaultValue: DataTypes.UUIDV4,
+        },
 
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
 
-    });
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+
+        refresh_token: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+    },
+    {
+        tableName: "users",
+        timestamps: true,
+        createdAt: "created_at",
+        updatedAt: "updated_at",
+    }
+);
+
+export default User;
